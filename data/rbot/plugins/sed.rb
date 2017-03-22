5d311a4871533f7561cf822e23d7855299a8c87b
LuK1337 <priv.luk@gmail.com>
2017-03-22 22:38:34 +0100

    Improve sed plugin

--- data/rbot/plugins/sed.rb
+++ data/rbot/plugins/sed.rb
@@ -10,51 +10,63 @@
 class Sed < Plugin
   def initialize
     super
-    # create the multidimensional hash 
+
+    # create the multidimensional hash
     @amendlog = Hash.new
-#    @answers = [
-#                "voleva dire",
-#                ", nella sua sbataggine, intendeva",
-#                "si è sbagliato. Voleva dire",
-#               ]
   end
 
   def help(plugin, topic="")
     "Fix the previous sentence using regexp and a sed-like syntax. Supported delimiters are /|,! and the modifier \"g\". Grouping is supported via parens, and backreferencing is done via \\1 \\2 and so on. You don't have to directly address the bot. Examples: <nick>hello <nick>s/e/u/"
   end
-  
+
   def message(m)
-    return unless m.public? 
-    # log 
+    return unless m.public?
+
+    # log
     source = m.source
     channel = m.channel
-    stuff = m.message
-    if not @amendlog.has_key?(channel)
+    message = m.message
+
+    unless @amendlog.has_key?(channel)
       @amendlog[channel] = Hash.new
     end
-    oldstring = @amendlog[channel][source] 
-    newstring = oldstring
-    if m.message.match(/^s([\/|,!])(.*?)\1(.*?)\1(g?)/) then
+
+    unless @amendlog[channel].key?(source)
+      @amendlog[channel][source] = []
+    end
+
+    if m.message.match(/^s([\/|,!])(.*?)\1(.*?)\1(g?)/)
       target = Regexp.new($2)
       replace_with = $3
       global = $4
-      if (global == "")
-        newstring = oldstring.sub(target, replace_with)
-      else
-        newstring = oldstring.gsub(target, replace_with)
-      end
-#      sentence = @answers[rand(@answers.length)]
-      sentence = _("meant")
-      if (oldstring == newstring)
-        failreply = _("You did something wrong... Try s/you/me/ or tell me \"help sed\"")
-        m.reply("#{source}: #{failreply}")
-        return
-      end
-      m.reply("#{source} #{sentence}: \"#{newstring}\"", :nick => false)
+
+      @amendlog[channel][source].reverse.each { |currentMessage|
+        if global == ""
+          newstring = currentMessage.sub(target, replace_with)
+        else
+          newstring = currentMessage.gsub(target, replace_with)
+        end
+
+        if newstring != currentMessage
+          m.reply("#{source} #{_("meant")}: \"#{newstring}\"", :nick => false)
+
+          @amendlog[channel][source].clear
+
+          return
+        end
+      }
+
+      failreply = _("You did something wrong... Try s/you/me/ or tell me \"help sed\"")
+      m.reply("#{source}: #{failreply}")
+
       return
     end
-    @amendlog[channel][source] = stuff
+
+    if @amendlog[channel][source].length >= 5
+      @amendlog[channel][source].shift
+    end
+
+    @amendlog[channel][source].push(message)
   end
 end
 plugin = Sed.new
-
